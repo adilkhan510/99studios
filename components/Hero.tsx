81e2573ae5dea22ea414066c8761e6b5675c4400
Adil Khan <54783965+Adilkhan510@users.noreply.github.com>
2023-06-28 22:52:14 -0700

    chore: add framer motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,25 @@
+'use client';
+
 import Container from './Container';
+import { motion } from 'framer-motion';
+
+const typeWriterEffect = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.09,
+    },
+  },
+};
+const typeWriterChar = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 interface HeroProps {
   title: string;
@@ -10,7 +31,22 @@ export default function Hero({ title, subtitle, description }: HeroProps) {
   return (
     <Container>
       <div className='flex w-[1100px] h-[600px] justify-center items-center flex-col m-auto space-y-4'>
-        <h1 className='text-8xl mb-10'>{title}</h1>
+        <motion.h1
+          variants={typeWriterEffect}
+          initial='hidden'
+          animate='visible'
+          className='text-8xl mb-10'
+        >
+          {title.split('').map((char, index) => (
+            <motion.span
+              key={char + '-' + index}
+              variants={typeWriterChar}
+              className='inline-block'
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.h1>
         <h2 className='text-md '>{subtitle}</h2>
         <p className='text-sm '>{description}</p>
       </div>
