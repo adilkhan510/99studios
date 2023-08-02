e49b5a76cfc86a70bd775a973156f27924903ecc
Adil Khan <54783965+Adilkhan510@users.noreply.github.com>
2023-08-01 19:52:15 -0700

    feat: add husky

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Container from './ui/Container';
-import SectionHead from './SectionHead';
+import SectionHead from './ui/SectionHead';
 
 const publishedBlogEntries = [
   {
@@ -32,38 +32,38 @@ export default function Blog() {
   return (
     <Container>
       <SectionHead
-        title='Our Blog'
-        description='We write about building startups and thoughts going on our mind.'
+        title="Our Blog"
+        description="We write about building startups and thoughts going on our mind."
       />
-      <div className='mt-16'>
-        <ul className='grid gap-16 max-w-4xl mx-auto'>
+      <div className="mt-16">
+        <ul className="grid gap-16 max-w-4xl mx-auto">
           {publishedBlogEntries.map((blogPostEntry, index) => (
             <li key={blogPostEntry.slug}>
               <a href={`/blog/${blogPostEntry.slug}`}>
-                <div className='grid md:grid-cols-2 gap-5 md:gap-10 items-center'>
+                <div className="grid md:grid-cols-2 gap-5 md:gap-10 items-center">
                   <Image
                     src={blogPostEntry.image.src}
                     alt={blogPostEntry.image.alt}
                     width={800}
                     height={450}
-                    className='rounded-md'
+                    className="rounded-md"
                   />
                   <div>
-                    <span className='text-blue-400 uppercase tracking-wider text-sm font-medium'>
+                    <span className="text-blue-400 uppercase tracking-wider text-sm font-medium">
                       {blogPostEntry.category}
                     </span>
 
-                    <h2 className='text-3xl font-semibold leading-snug tracking-tight mt-1 '>
+                    <h2 className="text-3xl font-semibold leading-snug tracking-tight mt-1 ">
                       {blogPostEntry.title}
                     </h2>
 
-                    <div className='flex gap-2 mt-3'>
-                      <span className='text-gray-400'>
+                    <div className="flex gap-2 mt-3">
+                      <span className="text-gray-400">
                         {blogPostEntry.author}
                       </span>
-                      <span className='text-gray-400'>• </span>
+                      <span className="text-gray-400">• </span>
                       <time
-                        className='text-gray-400'
+                        className="text-gray-400"
                         dateTime={new Date(
                           blogPostEntry.publishDate
                         ).toISOString()}
